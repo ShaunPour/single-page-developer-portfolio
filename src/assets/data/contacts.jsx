5a0aa47d05ec8cfbd45b4ea3b06f15904336cf9e
Shaun Pour <scpour@gmail.com>
2023-11-13 08:59:21 -0500

    Update contacts.jsx

--- src/assets/data/contacts.jsx
+++ src/assets/data/contacts.jsx
@@ -1,18 +1,18 @@
 export default [
     {
-        image: '../../../public/images/icon-github.svg',
+        image: '..\..\..\public\images\icon-github.svg',
         url: 'https://github.com/DarkPhoenixNinja92',
         name: 'Github',
         id: 0
     },
     {
-        image: '../../../public/images/icon-frontend-mentor.svg',
+        image: '..\..\..\public\images\icon-frontend-mentor.svg',
         url: 'https://www.frontendmentor.io/profile/DarkPhoenixNinja92',
         name: 'Frontend Mentor',
         id: 1
     },
     {
-        image: '../../../public/images/icon-linkedin.svg',
+        image: '..\..\..\public\images\icon-linkedin.svg',
         url: 'https://www.linkedin.com/in/shaun-pour/',
         name: 'LinkedIn',
         id: 2

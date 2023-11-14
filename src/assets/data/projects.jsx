4d2a94692ce28aa37d8f6234770a39cdb2d85a38
Shaun Pour <scpour@gmail.com>
2023-11-14 09:09:48 -0500

    Update projects.jsx

--- src/assets/data/projects.jsx
+++ src/assets/data/projects.jsx
@@ -4,7 +4,7 @@ export default [
         stack: ['html', 'css', 'javascript'],
         repository: 'https://github.com/DarkPhoenixNinja92/TeamTreeHouse-FullStack-Unit-03-Interactive-Form',
         site: 'https://darkphoenixninja92.github.io/TeamTreeHouse-FullStack-Unit-03-Interactive-Form',
-        image: '../../../public/images/form-validation.png',
+        image: 'public/images/form-validation.png',
         id: 0
     },
     {
@@ -12,7 +12,7 @@ export default [
         stack: ['html', 'css', 'javascript'],
         repository: 'https://github.com/DarkPhoenixNinja92/Team-Treehouse-Unit-06-Game-Show-App',
         site: 'https://darkphoenixninja92.github.io/Team-Treehouse-Unit-06-Game-Show-App/',
-        image: '../../../public/images/game-show-js.png',
+        image: 'public/images/game-show-js.png',
         id: 1
     },
     {
@@ -20,7 +20,7 @@ export default [
         stack: ['html', 'css', 'javascript'],
         repository: 'https://github.com/DarkPhoenixNinja92/TeamTreeHouse-FullStack-Unit-02-Pagination',
         site: 'https://darkphoenixninja92.github.io/TeamTreeHouse-FullStack-Unit-02-Pagination',
-        image: '../../../public/images/pagination.jpg',
+        image: 'public/images/pagination.jpg',
         id: 2
     },
     {
@@ -28,7 +28,7 @@ export default [
         stack: ['html', 'css', 'javascript'],
         repository: 'https://github.com/DarkPhoenixNinja92/TeamTreeHouse-Unit-05-Interactive-Photo-Gallery',
         site: 'https://darkphoenixninja92.github.io/TeamTreeHouse-Unit-05-Interactive-Photo-Gallery',
-        image: '../../../public/images/Gallery.png',
+        image: 'public/images/Gallery.png',
         id: 3
     },
     {
@@ -36,7 +36,7 @@ export default [
         stack: ['html', 'css', 'javascript'],
         repository: 'https://github.com/DarkPhoenixNinja92/Techdegree-01-Quote-Generator',
         site: 'https://darkphoenixninja92.github.io/Techdegree-01-Quote-Generator',
-        image: '../../../public/images/Quotes.png',
+        image: 'public/images/Quotes.png',
         id: 4
     },
     {
@@ -44,7 +44,7 @@ export default [
         stack: ['html', 'css'],
         repository: 'https://github.com/DarkPhoenixNinja92/chat-app-illustration/',
         site: 'https://darkphoenixninja92.github.io/chat-app-illustration/',
-        image: '../../../public/images/Chat.jpg',
+        image: 'public/images/Chat.jpg',
         id: 5
     }
 ]

432584b2fc61d4d21610aa6eb638384cfea20f0f
Shaun Pour <scpour@gmail.com>
2023-11-14 09:10:29 -0500

    Update Biography.jsx

--- src/assets/components/Biography.jsx
+++ src/assets/components/Biography.jsx
@@ -1,7 +1,7 @@
 const Biography = () => {
     return (
         <>
-            <img src="../../../public/images/me.jpg" alt="headshot" className="me" />
+            <img src="images/me.jpg" alt="headshot" className="me" />
             <h2 className="intro-title">Nice to meet you! I&apos;m <span className="green-span">Shaun Pour</span>.</h2>
             <p className="bio">Nice to meet you! I&apos;m Shaun Pour. Based in the USA, I&apos;m a front-end developer
                 passionate about building accessible web apps that users love. My focus is front
